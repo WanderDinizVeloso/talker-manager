b7e8fcafa454b504d8f366da20f06e78b34f8e2e
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-10-28 11:22:48 -0300

    corrige as functions readFlies e writeFiles

--- utils/readWriteFile.js
+++ utils/readWriteFile.js
@@ -1,26 +1,30 @@
 const { promises: { readFile, writeFile } } = require('fs');
 
-const readFiles = async (file, next) => {
+const readFiles = async (file) => {
   try {
     const content = await readFile(file, 'utf8');
   
     return JSON.parse(content);
   } catch (error) {
-    return next(error);
+    return error;
   }
 };
 
-const writeFiles = async (file, content, next) => {
+const writeFiles = async (file, content) => {
   try {
     const readContentFile = await readFiles(file);
+
+    const fileLength = readContentFile.length;
+
+    const contentWithId = { ...content, id: fileLength + 1 };
+
+    readContentFile.push(contentWithId);
+      
+    await writeFile(file, JSON.stringify(readContentFile));
   
-    readContentFile.push(content);
-  
-    await writeFile(file, JSON.stringfy(readContentFile));
-  
-    return content;
+    return contentWithId;
   } catch (error) {
-    return next(error);
+    return error;
   }
 };
 
