5ce3662820e26d3c85cc367b3173c3fd3e1101df
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-10-28 00:07:41 -0300

    corrige parametro da function generateToken

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto');
 
-const generateToken = () => crypto.randomBytes(16).toString('hex');
+const generateToken = () => crypto.randomBytes(8).toString('hex');
 
 module.exports = generateToken;
