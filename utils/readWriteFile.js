b311a05e654d1ca8c6e2426462bc700c1f387ce9
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-10-28 14:31:51 -0300

    cria function writeFilesByIdDelete

--- utils/readWriteFile.js
+++ utils/readWriteFile.js
@@ -43,8 +43,23 @@ const writeFilesByIdEdit = async (file, content) => {
   }
 };
 
+const writeFilesByIdEditDelete = async (file, id) => {
+  try {
+    const readContentFile = await readFiles(file);
+
+    const newFile = readContentFile.filter((talker) => talker.id !== id);
+    
+    await writeFile(file, JSON.stringify(newFile));
+  
+    return readContentFile;
+  } catch (error) {
+    return error;
+  }
+};
+
 module.exports = {
   readFiles,
   writeFilesAdd,
   writeFilesByIdEdit,
+  writeFilesByIdEditDelete,
 };
