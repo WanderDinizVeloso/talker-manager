ea64fd36e60fe7518821a118ad06d18016ed4ae2
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-10-28 00:33:30 -0300

    conclui requisito 3 - Crie o endpoint POST /login

--- controllers/login.js
+++ controllers/login.js
@@ -1,7 +1,21 @@
 const express = require('express');
 
-// const wrapper = require('../utils/wrapper');
+const wrapper = require('../utils/wrapper');
+const generateToken = require('../utils/generateToken');
+
+const { isValidPassword, isValidEmail } = require('../middlewares/validations');
+
+const login = async (_req, res, _next) => {
+  const token = generateToken();
+
+  return res.status(200).json({ token: `${token}` });
+};
 
 const router = express.Router({ mergeParams: true });
+
+router.post('/',
+  wrapper(isValidEmail),
+  wrapper(isValidPassword),
+  wrapper(login));
 
 module.exports = router;
