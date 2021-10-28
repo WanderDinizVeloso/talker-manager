e11c596e4d759684c1b274609e65e384f597e0b4
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-10-27 23:22:19 -0300

    conclui requisito 2 - Crie o endpoint GET /talker/:id

--- controllers/talker.js
+++ controllers/talker.js
@@ -11,8 +11,24 @@ const find = async (_req, res, _next) => {
   return res.status(200).json(findTalker);
 };
 
+const findByid = async (req, res, next) => {
+  const findTalker = await readFiles(FILE);
+  const { id } = req.params;  
+  const talker = findTalker.find((talk) => talk.id === parseInt(id, 10));
+
+  if (!talker) {
+    return next({
+      status: 404,
+      message: 'Pessoa palestrante não encontrada',
+    });
+  } 
+  
+  return res.status(200).json(talker);
+};
+
 const router = express.Router({ mergeParams: true });
 
 router.get('/', wrapper(find));
+router.get('/:id', wrapper(findByid));
 
 module.exports = router;
