e71a179945d8fad85b2abdaa8ed5997d56ffe2a8
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-10-29 16:46:09 -0300

    refatora function checkWatchedAt

--- middlewares/validations/isValidTalk/checkWatchedAt.js
+++ middlewares/validations/isValidTalk/checkWatchedAt.js
@@ -2,4 +2,6 @@ function checkWatchedAt(param) {
   return /([0-2][0-9]|3[0-1])\/(0[0-9]|1[0-2])\/[0-9]{4}$/.test(param);
 }
 
+// regex format date: https://www.regextester.com/99555
+
 module.exports = checkWatchedAt;
